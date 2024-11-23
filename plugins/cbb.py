6fe38ba98c360c383bde5b19c59f45b6e3db759c
kawukiandrew7777 <60316867+kawukiandrew7777@users.noreply.github.com>
2024-11-23 10:08:55 +0300

    Add files via upload

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -3,14 +3,14 @@
 from pyrogram import __version__
 from bot import Bot
 from config import OWNER_ID
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creater : @lflixadmin\n\n○ Language : Python\n\n○ Python Library : <a href='https://docs.pyrogram.org/'> pyrogram </a></b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Channel : @katandika2022\n○ Support Group : @katandika2023</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
